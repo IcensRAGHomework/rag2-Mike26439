4395dd7427bba192ebd2084bfd9cf769ff113263
Mike26439 <mike_hsu@asus.com>
2025-01-29 19:00:43 +0800

    HW2_01 0129

--- student_assignment.py
+++ student_assignment.py
@@ -7,7 +7,19 @@ q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
-    pass
+    loader = PyPDFLoader(q1_pdf)
+    documents = loader.load()
+    text_splitter = CharacterTextSplitter(
+        separator="\n\n",
+        chunk_size=1000,
+        chunk_overlap=0,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    split_documents = text_splitter.split_documents(documents)
+    return split_documents[-1]
 
 def hw02_2(q2_pdf):
     pass
+
+#print(hw02_1(q1_pdf))
