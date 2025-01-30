7a86a87505cf17429fcb01e9eff646a8bd43f7c4
Mike26439 <mike_hsu@asus.com>
2025-01-30 18:50:14 +0800

    HW2 02 0130

--- student_assignment.py
+++ student_assignment.py
@@ -20,6 +20,23 @@ def hw02_1(q1_pdf):
     return split_documents[-1]
 
 def hw02_2(q2_pdf):
-    pass
+    loader = PyPDFLoader(q2_pdf)
+    documents = loader.load()
 
-#print(hw02_1(q1_pdf))
+    full_text = "\n".join(doc.page_content for doc in documents)
+
+    recursive_text_splitter = RecursiveCharacterTextSplitter(
+        separators=[r"\n.*第.{0,}章.{0,}?\n",  # 第x章
+            r"第.{0,}\d{1,}.{0,}條.{0,}\n"  # 第 x 條
+        ],
+        is_separator_regex=True,
+        chunk_size = 10,
+        chunk_overlap = 0,
+        length_function = len,
+        add_start_index = False,
+    )
+
+    split_documents = recursive_text_splitter.split_text(full_text)
+    return len(split_documents)
+
+print(hw02_2(q2_pdf))
